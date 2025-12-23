fad9e2b13c9a6c0e7075ee60d03e80bfbd8a6528
Rob Raymundo <robgodwinr@gmail.com>
2025-12-23 12:46:20 +0800

    Fix bug in checkout logic

--- src/pos/Main.java
+++ src/pos/Main.java
@@ -2,6 +2,6 @@ package pos;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("POS System Starting...");
+        System.out.println("POS System Starting NOW...");
     }
 }
