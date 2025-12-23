e5aae21cd97d211de3cd8605fd49cdf7aaf67a01
Rob Raymundo <robgodwinr@gmail.com>
2025-12-23 13:14:03 +0800

    Merry christmas

--- src/pos/Main.java
+++ src/pos/Main.java
@@ -3,5 +3,6 @@ package pos;
 public class Main {
     public static void main(String[] args) {
         System.out.println("POS System Starting NOW...");
+        System.out.println("MERRY CHRISTMAS!");
     }
 }
