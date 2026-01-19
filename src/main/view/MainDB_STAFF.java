89d8ace704e3f7fed94c19a941e42b6ea6d9b7a1
Rob Raymundo <robgodwinr@gmail.com>
2026-01-20 00:15:30 +0800

    added logout button and changepassword frame

--- src/main/view/MainDB_STAFF.java
+++ src/main/view/MainDB_STAFF.java
@@ -194,7 +194,8 @@ sixthClickable.setBounds(0, 50, imgW6, imgH6);
 sixthClickable.addMouseListener(new MouseAdapter() {
     @Override
     public void mousePressed(MouseEvent e) {
-        // Action HEREE
+        UserDetailFrame userDetailFrame = new UserDetailFrame(user);
+        userDetailFrame.setVisible(true);// Action HEREE
     } 
     @Override
     public void mouseEntered(MouseEvent e) {
