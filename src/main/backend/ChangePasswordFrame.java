5de453bf1192255c13f524a6a286b5dd984406ec
Rob Raymundo <robgodwinr@gmail.com>
2026-01-20 05:37:32 +0800

    erp

--- src/main/backend/ChangePasswordFrame.java
+++ src/main/backend/ChangePasswordFrame.java
@@ -1,46 +1,55 @@
 package main.backend;
 
-import javax.swing.*;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.HeadlessException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import main.model.User;
 
-public class ChangePasswordFrame extends JFrame{
+public class ChangePasswordFrame extends JFrame {
     User user;
 
-    public ChangePasswordFrame(User user){
-        this.user = user;
+    public ChangePasswordFrame(User user) throws HeadlessException {
+        //this.user = user;
         setTitle("Change Password");
         setSize(400, 300);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
 
-        JLabel CurrentPasswordLabel = new JLabel("Current Password: ");
-        JTextField CurrentPassword = new JTextField();
-        JLabel NewPasswordLabel = new JLabel("New Password: ");
-        JTextField NewPassword = new JTextField();
-        JLabel ConfirmPasswordLabel = new JLabel("Confirm Password: ");
-        JTextField ConfirmPassword = new JTextField();
-        JButton ChangePasswordButton = new JButton("Change Password");
-        JButton CancelButton = new JButton("Cancel");
-        JPanel panel = new JPanel();
-        panel.add(CurrentPasswordLabel);
-        panel.add(CurrentPassword);
-        panel.add(NewPasswordLabel);
-        panel.add(NewPassword);
-        panel.add(ConfirmPasswordLabel);
-        panel.add(ConfirmPassword);
-        panel.add(ChangePasswordButton);
-        panel.add(CancelButton);
+        JLabel currentPasswordLabel = new JLabel("Current Password: ");
+        currentPasswordLabel.setFont(new Font("Arial", Font.BOLD, 12));
 
+        JTextField currentPassword = new JTextField();
+        JLabel newPasswordLabel = new JLabel("New Password: ");
+        newPasswordLabel.setFont(new Font("Arial", Font.BOLD, 12));
 
+        JTextField newPassword = new JTextField();
+        JLabel confirmPasswordLabel = new JLabel("Confirm Password: ");
+        confirmPasswordLabel.setFont(new Font("Arial", Font.BOLD, 12));
 
+        JTextField confirmPassword = new JTextField();
+        JButton changePasswordButton = new JButton("Change Password");
+        JButton cancelButton = new JButton("Cancel");
+        JPanel panel = new JPanel(new GridLayout(4, 2, 10, 10));
 
-
-
-
+        panel.add(currentPasswordLabel);
+        panel.add(currentPassword);
+        panel.add(newPasswordLabel);
+        panel.add(newPassword);
+        panel.add(confirmPasswordLabel);
+        panel.add(confirmPassword);
+        panel.add(changePasswordButton);
+        panel.add(cancelButton);
 
         add(panel);
         setVisible(true);
     }
-    
+
+
 }
